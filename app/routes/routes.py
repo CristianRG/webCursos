ca4f8e34715d156349a013c153f0530e7530a328
CristianRG <cristianrizogomez890@gmail.com>
2022-12-31 20:23:57 -0600

    second commit. News updates

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,7 +1,14 @@
 from flask import redirect, render_template, url_for
-from templates import app
+from run import app
 
 @app.route('/')
 def Index():
-    return render_template('app/templates/index.html')
+    return render_template('index.html')
 
+@app.route('/courses/')
+def getCourses():
+    return render_template('courses.html')
+
+@app.route('/courses/<string:course>')
+def getCourse(course):
+    return render_template('courses/{}/course-{}.html'.format(course, course))
--- app/run.py
+++ app/run.py
@@ -1,4 +1,9 @@
-from templates import app
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config.developmentConfig')
+
+from routes import *
 
 if __name__ == "__main__":
     app.run()
